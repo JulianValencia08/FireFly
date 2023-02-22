308fb34f9a62b111d06601e4839f8b65f9159f12
julianv <julianv302@gmail.com>
2023-02-22 10:53:26 -0500

    Conexion con MongoDB y superuser creado

--- Match/models.py
+++
@@ -1,5 +0,0 @@
-from djongo import models
-
-class MyModel(models.Model):
-    my_field = models.CharField(max_length=100)
-    my_array_field = models.ArrayField(model_container=models.CharField(max_length=50))
